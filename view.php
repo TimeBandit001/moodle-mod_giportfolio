109bec3cde352c74791ee09ec9b82e3d6ecc4c2c
Marc Charbonneau <mcharbonneau@dawsoncollege.qc.ca>
2025-10-09 16:57:27 -0400

    when calling format_text, the second parameter (format) must be an integer, not the intro text

--- view.php
+++ view.php
@@ -113,7 +113,7 @@ echo $OUTPUT->heading(format_string($giportfolio->name));
 echo $OUTPUT->box_start('generalbox giportfolio_content');
 
 $intro = file_rewrite_pluginfile_urls($giportfolio->intro, 'pluginfile.php', $context->id, 'mod_giportfolio', 'intro', '');
-echo format_text($intro, $giportfolio->intro, array('noclean' => true, 'context' => $context));
+echo format_text($intro, $giportfolio->introformat, array('noclean' => true, 'context' => $context));
 
 $usercontribution = 0;
 if ($allowedit) {
